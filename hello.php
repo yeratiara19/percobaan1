93bd3111ef13fd9f0653ce9620fb5e1fa89fc784
yeratiara19 <yera.tiara@yahoo.co.id>
2015-11-15 13:06:19 +0700

    Ubah hello.php & tambah Hallo.php

--- hello.php
+++ hello.php
@@ -1,3 +1,14 @@
-<?php
-echo "Hello Azure!";
-?>
+<html>
+<head>
+	<title>Web App Sederhana</title>
+</head>
+<body>
+	<form action="hello.php" method="POST">
+		Nama : <input type="text" name="nama"/>
+		<br>
+		Email : <input type="email" name="email"/>
+		<br>
+		<input type="submit" value="Enter"/>
+	</form>
+</body>
+</html>
